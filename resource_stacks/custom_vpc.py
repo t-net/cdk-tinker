2f06cf8e36805c6b65e895e9c21aa93e58882ee3
Tinette Nolan <tinette.nolan@vw-dilab.com>
2021-09-24 15:40:14 +0100

    Add custom vpc
    - Remove docker spec

--- resource_stacks/custom_vpc.py
+++ resource_stacks/custom_vpc.py
@@ -1,9 +1,39 @@
 from aws_cdk import core
-
+from aws_cdk.aws_ec2 import (
+    Vpc,
+    SubnetConfiguration,
+    SubnetType
+)
 
 class CustomVpcStack(core.Stack):
 
     def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        custom_vpc = Vpc(
+            self,
+            "Vpc",
+            max_azs=3,
+            cidr="10.0.0.0/16",
+            subnet_configuration=[
+                SubnetConfiguration(
+                    cidr_mask=24,
+                    name="ingress",
+                    subnet_type=SubnetType.PUBLIC
+                ),
+                SubnetConfiguration(
+                    cidr_mask=24,
+                    name="app",
+                    subnet_type=SubnetType.ISOLATED
+                ),
+            ]
+        )
+
+        core.Tag.add(custom_vpc, "Author", "Tinette")
+
+        core.CfnOutput(
+            self,
+            "Vpc",
+            value=custom_vpc.vpc_id,
+            export_name="custom_vpc_id"
+        )
